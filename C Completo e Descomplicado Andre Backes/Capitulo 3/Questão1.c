1ca56e6abe09fc2d1becb38a774eb65217705e39
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-03-03 14:08:07 -0300

    questão1.c

--- C Completo e Descomplicado Andre Backes/Capitulo 3/Questão1.c
+++ C Completo e Descomplicado Andre Backes/Capitulo 3/Questão1.c
@@ -1,1 +1,18 @@
+// 1) Faça um programa que leia um número inteiro e retorne seu
+// antecessor e seu sucessor.
 
+#include <stdio.h>
+
+int main(){
+
+    int num, ant, sucs;
+    scanf("%d", &num);
+
+    num = (ant = num -1, sucs = num +1);
+
+    printf("Antecessor: %d\n", ant--);
+
+    printf("Sucessor: %d", sucs++);
+
+    return 0;
+}
