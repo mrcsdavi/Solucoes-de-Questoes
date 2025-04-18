a4b3ead7f979845476fd8adb51f48be387b1153c
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-04-18 16:14:23 -0300

    Create Questao8.c

---
+++ C Completo e Descomplicado Andre Backes/Capitulo 5/Questao8.c
@@ -0,0 +1,22 @@
+/* 8) Faça um programa que leia 10 inteiros e imprima sua média.*/
+
+#include <stdio.h>
+
+int main(){
+
+    int num, media, soma = 0;
+
+    int i = 1;
+    printf("Digite 10 numeros: \n");
+    while(i <= 10){
+        scanf("%d", &num);
+        soma = soma + num;
+        if(i == 10) media = soma/i; 
+        i++;
+    }
+    printf("soma: %d i: %d\n", soma, i);
+
+    printf("%d", media);
+
+    return 0;
+}
