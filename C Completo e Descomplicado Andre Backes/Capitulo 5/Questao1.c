94090a9b30967e5d38be58d6138c70828acac648
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-04-17 23:56:24 -0300

    Update Questao1.c

--- C Completo e Descomplicado Andre Backes/Capitulo 5/Questao1.c
+++ C Completo e Descomplicado Andre Backes/Capitulo 5/Questao1.c
@@ -1,1 +1,18 @@
+/*1) Faça um programa que leia um número inteiro positivo N e
+ imprima todos os números naturais de 0 até N em ordem
+ crescente.*/
 
+ #include <stdio.h>
+
+ int main(){
+
+    int N;
+    printf("Digite um numero inteiro positivo: \n");
+    scanf("%d", &N);
+
+    if(N > 0){
+        for(int i = 0; i <= N; i++) printf("%d\n", i);
+    }
+
+    return 0;
+ }
