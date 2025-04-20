00e8b79d239dba552e1a817dc1ea1c4b5f072dd1
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-04-20 11:49:57 -0300

    Create Questao17.c

---
+++ C Completo e Descomplicado Andre Backes/Capitulo 5/Questao17.c
@@ -0,0 +1,26 @@
+/*17) Escreva um programa que leia um número inteiro positivo N
+ e em seguida imprima N linhas do chamado triângulo de
+ Floyd:
+ 1
+ 2  3 
+ 4  5  6
+ 7  8  9  10
+ 11 12 13 14 15
+ 16 17 18 19 20 21*/
+
+#include <stdio.h>
+
+int main(){
+
+    int N, num = 1;
+    
+    scanf("%d", &N);
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= i; j++){
+            printf("%d ", num);
+            num++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
