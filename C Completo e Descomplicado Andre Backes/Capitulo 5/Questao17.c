5619d6fa226831477e4d520cde19cf1d7dd1ad6a
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-04-20 11:51:42 -0300

    Update Questao17.c

--- C Completo e Descomplicado Andre Backes/Capitulo 5/Questao17.c
+++ C Completo e Descomplicado Andre Backes/Capitulo 5/Questao17.c
@@ -15,12 +15,15 @@ int main(){
     int N, num = 1;
     
     scanf("%d", &N);
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= i; j++){
-            printf("%d ", num);
-            num++;
+    if(N > 0){
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= i; j++){
+                printf("%d ", num);
+                num++;
+            }
+            printf("\n");
         }
-        printf("\n");
     }
+    
     return 0;
 }
