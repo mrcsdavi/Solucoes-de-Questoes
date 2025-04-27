d5ba82aaace89d027ff811e731965fc75e1c638c
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-04-27 20:54:59 -0300

    Update Questao12.c

--- C Completo e Descomplicado Andre Backes/Capitulo 6 - Vetores e Matrizes/Matrizes/Questao12.c
+++ C Completo e Descomplicado Andre Backes/Capitulo 6 - Vetores e Matrizes/Matrizes/Questao12.c
@@ -6,7 +6,7 @@
 
  int main(){
 
-    int soma = 0, matriz[6][6];
+    int increment = 1, soma = 0, matriz[6][6];
 
    for(int i = 0; i < 6; i++){
       for(int j = 0; j < 6; j++){
@@ -17,9 +17,10 @@
 
    for(int i = 0; i < 6; i++){
       for(int j = 0; j < 6; j++){
-         if((i == 1 && j == 0) || (i == 2 && j == 1) || (i == 3 && j == 2) || (i == 4 && j == 3) || (i == 5 && j == 4))
+         if(i + j == increment)
             soma += matriz[i][j];
       }
+      increment += 2;
    }
    printf("Soma = %d\n", soma);
 
