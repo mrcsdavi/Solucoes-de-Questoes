502499f21eeea9d152814f4e7c5854ddcc53daab
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-09-11 16:58:37 -0300

    Create Questao10.c

---
+++ C Completo e Descomplicado Andre Backes/Capitulo 7 - Array de Caracteres/Questao10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[50];
+
+    printf("Digite a string: ");
+    fgets(str, 50, stdin); // mais seguro que gets
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+    }
+
+    printf("String em maiúsculas: %s", str);
+
+    return 0;
+}
