6a41b822fb7c4f463856a8c86ce148b4f1afda01
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-10-06 00:38:38 -0300

    Update Questao1.c

--- C Completo e Descomplicado Andre Backes/Capitulo 9 - Funcoes/Passagem por valor/Questao1.c
+++ C Completo e Descomplicado Andre Backes/Capitulo 9 - Funcoes/Passagem por valor/Questao1.c
@@ -1,1 +1,20 @@
+/* 1) Escreva uma função que receba por parâmetro dois números e
+ retorne o maior deles.*/
 
+#include <stdio.h>
+
+int maior(int a, int b){
+    if(a > b) return a;
+    else return b;
+}
+
+int main(){
+    int x, y;
+
+    printf("Digite dois numeros: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Maior numero é o: %d", maior(x,y));
+
+    return 0;
+}
