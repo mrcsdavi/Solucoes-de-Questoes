bc3ca3b51b3ee6d3deeb5caaf21443f725030c3c
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-10-06 12:16:29 -0300

    Update Questao6.c

--- C Completo e Descomplicado Andre Backes/Capitulo 9 - Funcoes/Passagem por valor/Questao6.c
+++ C Completo e Descomplicado Andre Backes/Capitulo 9 - Funcoes/Passagem por valor/Questao6.c
@@ -1,1 +1,26 @@
+/* 6) Escreva uma função que receba o peso (quilos) e a altura (metros) de uma
+ pessoa. Calcule e retorne o IMC (Índice de Massa Corporal) dessa pessoa*/
 
+#include <stdio.h>
+
+struct pessoa{
+    float peso;
+    float altura;
+};
+
+typedef struct pessoa p;
+
+float Calc_IMC(float peso, float altura){
+    return peso/(altura*altura);
+}
+
+int main(){
+    p p1;
+
+    printf("Digite o seu peso e a sua altura: ");
+    scanf("%f %f", &p1.peso, &p1.altura);
+    
+    printf("Calculo do IMC eh: %.2f", Calc_IMC(p1.peso, p1.altura));
+
+    return 0;
+}
