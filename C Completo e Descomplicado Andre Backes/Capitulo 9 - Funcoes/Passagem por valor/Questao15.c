fcfaffe20367e40a6e4253c118c83d42edac65fb
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-10-06 21:46:01 -0300

    Create Questao15.c

---
+++ C Completo e Descomplicado Andre Backes/Capitulo 9 - Funcoes/Passagem por valor/Questao15.c
@@ -0,0 +1,18 @@
+/*15) Elabore uma função que receba como parâmetro um valor
+ inteiro n e gere como saída n linhas com pontos de
+ exclamação, conforme o exemplo a seguir, em que usamos
+ n = 5:
+ !
+ !!
+ !!!
+ !!!!
+ !!!!!*/
+
+#include <stdio.h>
+
+int main(){
+
+
+
+    return 0;
+}
