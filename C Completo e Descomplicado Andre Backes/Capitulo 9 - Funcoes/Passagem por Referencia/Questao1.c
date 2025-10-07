9feb6eb9aafc29376f20ec80dec6a0990ac5e2a8
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-10-07 20:43:07 -0300

    Update Questao1.c

--- C Completo e Descomplicado Andre Backes/Capitulo 9 - Funcoes/Passagem por Referencia/Questao1.c
+++ C Completo e Descomplicado Andre Backes/Capitulo 9 - Funcoes/Passagem por Referencia/Questao1.c
@@ -1,1 +1,24 @@
+/*1) Escreva uma função que, dado um número real passado como parâmetro,
+ retorne a parte inteira e a parte fracionária desse número por referência*/
 
+#include <stdio.h>
+
+void retornar(float num, int *inteira, float *frac){
+    *inteira = (int) num;
+    *frac = num - *inteira;
+}
+
+int main(){
+    float a, partFrac;
+    int partInteira;
+
+    printf("Digite um numero: ");
+    scanf("%f", &a);
+
+    retornar(a, &partInteira, &partFrac);
+
+    printf("Parte inteira: %d\n", partInteira);
+    printf("Parte fracionaria: %.2f\n", partFrac);
+
+    return 0;
+}
