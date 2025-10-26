815f4aa8abecd1b3ef0219a9c25e221101c38a9d
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-10-26 10:41:34 -0300

    Update questao1.c

--- C Completo e Descomplicado Andre Backes/Capitulo 9 - Funcoes/Recursao/questao1.c
+++ C Completo e Descomplicado Andre Backes/Capitulo 9 - Funcoes/Recursao/questao1.c
@@ -1,1 +1,25 @@
+/* 1) Escreva uma função recursiva que calcule a soma dos
+ primeiros n cubos:*/
 
+#include <stdio.h>
+#include <math.h>
+
+int somaCubos(int n){
+    if(n == 0){
+        return 0;
+    }else{
+        return pow(n, 3)+somaCubos(n-1);
+    }
+}
+
+int main(){
+    int a;
+
+    printf("Digite um numero: ");
+    scanf("%d", &a);
+
+    printf("Soma resultante: %d", somaCubos(a));
+
+
+    return 0;
+}
