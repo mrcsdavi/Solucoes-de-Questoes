7af6f74afa192e31b7b8aa5f9bb7a84f0ee82537
Marcos Davi Sena <155929590+mrcsdavi@users.noreply.github.com>
2025-11-18 16:40:33 -0300

    Create questao1.c

---
+++ C Completo e Descomplicado Andre Backes/Capitulo 11 /questao1.c
@@ -0,0 +1,14 @@
+/*1) Escreva um programa que mostre o tamanho em byte que cada tipo de
+ dados ocupa na memória: char, int, float, double.*/
+
+#include <stdio.h>
+
+int main(){
+
+    printf("%d\n", sizeof(char));
+    printf("%d\n", sizeof(int));
+    printf("%d\n", sizeof(float));
+    printf("%d\n", sizeof(double));
+
+    return 0;
+}
